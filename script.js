a639fe3e6f987cf210ddaa000a88deaea043336e
Jade <ch.jade427@gmail.com>
2026-06-16 21:49:52 -0700

    Add grid rezie button and functionality

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#resize-btn");
+button.addEventListener("click", () => {
+  let size = Number(prompt("Enter grid size (1 - 100): "));
 
-for (let i = 0; i < 250; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
+  while (size > 100 || size < 1 || isNaN(size)) {
+    size = Number(prompt("Enter grid size (1 - 100): "));
+  }
 
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "black";
-  });
+  container.innerHTML = "";
+  createGrid(size);
+});
 
-  container.appendChild(square);
+function createGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.style.width = `${640 / size}px`;
+    square.style.height = `${640 / size}px`;
+
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "black";
+    });
+
+    container.appendChild(square);
+  }
 }
+
+createGrid(16);
