48707ee962dc33a338c7695e777fb51854189acc
Jade <ch.jade427@gmail.com>
2026-06-16 21:28:53 -0700

    Add hover drawing effect

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 250; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+
+  square.addEventListener("mouseenter", () => {
+    square.style.backgroundColor = "black";
+  });
+
   container.appendChild(square);
 }
